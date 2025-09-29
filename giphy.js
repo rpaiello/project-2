c102909946387e185b4493b6b23834a423dcc185
rpaiello <rpaiello@gmail.com>
2025-09-29 18:44:00 -0400

    IT DISPLAYS THE GIFS NOW! (Although it has a ways to go aesthetics-wise)

--- giphy.js
+++ giphy.js
@@ -1,6 +1,6 @@
 const gifContainer = document.querySelector('.js-gif-display');
-const searchBar = document.querySelector('#searchBox');
-const ratingSelect = document.querySelector('#rating')
+const searchForm = document.querySelector('#searchBox');
+const searchBar = document.querySelector('#searchBar');
 let content = null;
 
 // Query object:
@@ -9,17 +9,46 @@ let exampleQuery = {
     r : null, //Null for none, otherwise 'g', 'pg', 'pg13', 'r'.
     o : false, //Random gif offset (if true)
 }
-function search(event) {
-    event.preventDefault();
-    loadGifs({
+
+function getRating() {
+    if (document.querySelector('#dc').checked) return null;
+    else {
+        for (let rating of ['g','pg','pg13','r']) {
+            if (document.querySelector(`#${rating}`).checked) {
+                return rating;
+            }
+        }
+    }
+}
+
+function search() {
+    let thisQuery = {
         q : searchBar.value,
-    })
+        r : getRating(),
+        o : false,
+    };
+    console.log(`SEARCH QUERY: ${thisQuery}`);
+    loadGifs(thisQuery);
+}
+
+function clearLoadedContent(element) {
+    element.innerHTML = "";
 }
 
 async function loadGifs(query) {
-    console.log(`https://api.giphy.com/v1/gifs/search?api_key=w5p17kHyXSUJ5yL2gftyNNkDEyKBDm4g&q=${encodeURIComponent(query.q)}&limit=25&offset=${query.o ? "25" : "0"}${query.r ? "&rating=" + query.r : ""}&lang=en&bundle=clips_grid_picker`);
     let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=w5p17kHyXSUJ5yL2gftyNNkDEyKBDm4g&q=${encodeURIComponent(query.q)}&limit=25&offset=${query.o ? "25" : "0"}${query.r ? "&rating=" + query.r : ""}&lang=en&bundle=clips_grid_picker`);
     let gifs = await response.json();
     console.log(gifs.data);
+    clearLoadedContent(gifContainer);
+    for (let gif of gifs.data) {
+        let output = document.createElement('img');
+        output.setAttribute('src', gif.images.fixed_width.webp);
+        output.classList.add('isblock');
+        gifContainer.appendChild(output);
+    }
 }
-loadGifs({q:"Minecraft creeper", r:null, o:true});
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    search();
+})
