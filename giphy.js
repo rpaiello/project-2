ebbe8b93831c93900fbf1594a627fe741aae0bd7
rpaiello <rpaiello@gmail.com>
2025-09-30 12:44:18 -0400

    WOW im on a roll. Might fall short of the "15 commit" requirement ha ha

--- giphy.js
+++ giphy.js
@@ -1,7 +1,7 @@
 const gifContainer = document.querySelector('.js-gif-display');
 const searchForm = document.querySelector('#searchBox');
 const searchBar = document.querySelector('#searchBar');
-let content = null;
+let gifs = null;
 
 // Query object:
 let exampleQuery = {
@@ -21,13 +21,32 @@ function getRating() {
     }
 }
 
+function getRandomBackgroundColor() {
+    return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+}
+
+function createGifInfo(gif) {
+    let title = document.createElement('p');
+    title.classList.add('cf', 'body', 'gif-info-text');
+    title.style.textShadow = `1px 1px ${getRandomBackgroundColor()}`;
+    console.log(getRandomBackgroundColor());
+    title.innerHTML = `${gif.title}`;
+    let info = document.createElement('p');
+    info.classList.add('ffx', 'info');
+    info.innerHTML = `uploaded by: <b>${gif.username ? gif.username : "(No username)"}</b><br>original dimensions: ${gif.images.original.width}x${gif.images.original.height}<br>imported on: ${gif.import_datetime}<br><a href="${gif.bitly_url}">view original on GIPHY</a>`;
+    let output = document.createElement('div');
+    output.classList.add('gif-info');
+    output.appendChild(title);
+    output.appendChild(info);
+    return output;
+}
+
 function search() {
     let thisQuery = {
         q : searchBar.value,
         r : getRating(),
         o : false,
     };
-    console.log(`SEARCH QUERY: ${thisQuery}`);
     loadGifs(thisQuery);
 }
 
@@ -36,14 +55,16 @@ function clearLoadedContent(element) {
 }
 
 async function loadGifs(query) {
-    let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=w5p17kHyXSUJ5yL2gftyNNkDEyKBDm4g&q=${encodeURIComponent(query.q)}&limit=25&offset=${query.o ? "25" : "0"}${query.r ? "&rating=" + query.r : ""}&lang=en&bundle=clips_grid_picker`);
-    let gifs = await response.json();
+    let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=w5p17kHyXSUJ5yL2gftyNNkDEyKBDm4g&q=${encodeURIComponent(query.q)}&limit=24&offset=${query.o ? "25" : "0"}${query.r ? "&rating=" + query.r : ""}&lang=en&bundle=clips_grid_picker`);
+    gifs = await response.json();
     console.log(gifs.data);
     clearLoadedContent(gifContainer);
     for (let gif of gifs.data) {
-        let output = document.createElement('img');
-        output.setAttribute('src', gif.images.fixed_width.webp);
-        output.classList.add('isblock');
+        let output = document.createElement('div');
+        output.innerHTML = `<img src="${gif.images.fixed_width.webp}" alt="${gif.alt_text}"></img>`;
+        output.classList.add('js-gif','gif');
+        let info = createGifInfo(gif);
+        output.appendChild(info);
         gifContainer.appendChild(output);
     }
 }
